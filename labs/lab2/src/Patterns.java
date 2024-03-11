e25bdc2bb694dd3e1f85e209a9bd5a3697ea2fc4
JazCz <2508413494@qq.com>
2024-03-11 21:33:19 +0800

    lab2updatep3

--- labs/lab2/src/Patterns.java
+++ labs/lab2/src/Patterns.java
@@ -1,2 +1,26 @@
 public class Patterns {
+    public void printPattern1(int n){
+        for(int i = 1; i<= n; i++)
+        {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    public void printPattern2(int n){
+        for(int i = n; i>=1; i--)
+        {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Patterns pt=new Patterns();
+        pt.printPattern1(Integer.parseInt(args[0]));
+        pt.printPattern2(Integer.parseInt(args[0]));
+    }
 }
